ad79a59f048c387958649984cf61e92d16da8fde
darkwarrior303 <69532073+darkwarrior303@users.noreply.github.com>
2022-06-21 23:09:15 +0530

    d-kart website

---
+++ js/main.js
@@ -0,0 +1,70 @@
+var heart = document.getElementById('heart1');
+var heartTwo = document.getElementById('heart2');
+var heartThree = document.getElementById('heart3');
+var heartFour = document.getElementById('heart4');
+var heartFive = document.getElementById('heart5');
+var heartSix = document.getElementById('heart6');
+var heartSeven = document.getElementById('heart7');
+
+function toggle1(){
+	if (heart.style.fill == "red") {
+		heart.style.fill = 'black'
+	}
+	else{
+		heart.style.fill = 'red'
+	}
+}
+
+function toggle2(){
+	if (heartTwo.style.fill == "red") {
+		heartTwo.style.fill = "black"
+	}
+	else{
+		heartTwo.style.fill = "red"
+	}
+}
+
+function toggle3(){
+	if (heartThree.style.fill == "red") {
+		heartThree.style.fill = "black"
+	}
+	else{
+		heartThree.style.fill = "red"
+	}
+}
+
+function toggle4(){
+	if (heartFour.style.fill == "red") {
+		heartFour.style.fill = "black"
+	}
+	else{
+		heartFour.style.fill = "red"
+	}
+}
+
+function toggle5(){
+	if (heartFive.style.fill == "red") {
+		heartFive.style.fill = "black"
+	}
+	else{
+		heartFive.style.fill = "red"
+	}
+}
+
+function toggle6(){
+	if (heartSix.style.fill == "red") {
+		heartSix.style.fill = "black"
+	}
+	else{
+		heartSix.style.fill = "red"
+	}
+}
+
+function toggle7(){
+	if (heartSeven.style.fill == "red") {
+		heartSeven.style.fill = "black"
+	}
+	else{
+		heartSeven.style.fill = "red"
+	}
+}
